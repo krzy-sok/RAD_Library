6b8f4627a15e4840e24cab48681b2e0fc85a6010
ksokol <01178595@pw.edu.pl>
2024-11-10 14:49:51 +0100

    removed unused crud on users

--- RAD biblioteka/Views/UsersController.cs
+++ RAD biblioteka/Views/UsersController.cs
@@ -52,125 +52,72 @@ namespace RAD_biblioteka.Views
             return View(user);
         }
 
-        [Authorize(Policy = "Librarian")]
-        // GET: Users/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
         // POST: Users/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Policy = "Librarian")]
-        public async Task<IActionResult> Create([Bind("Id,userName,firstName,lastName,email,phoneNumber,password")] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                user.password=  user.password;
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
 
-        // GET: Users/Edit/5
-        [Authorize(Policy = "Librarian")]
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.User == null)
-            {
-                return NotFound();
-            }
 
-            var user = await _context.User.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
+        //commented out for bind reference
 
-        // POST: Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Policy = "Librarian")]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,userName,firstName,lastName,email,phoneNumber,password")] User user)
-        {
-            if (id != user.Id)
-            {
-                return NotFound();
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Policy = "Librarian")]
+        //public async Task<IActionResult> Create([Bind("Id,userName,firstName,lastName,email,phoneNumber,password")] User user)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        user.password=  user.password;
+        //        _context.Add(user);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(user);
+        //}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
 
-        // GET: Users/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.User == null)
-            {
-                return NotFound();
-            }
 
-            var user = await _context.User
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+        //commented out for delete reference
 
-            return View(user);
-        }
+        //// GET: Users/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null || _context.User == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var user = await _context.User
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(user);
+        //}
 
         // POST: Users/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        [Authorize(Policy = "Librarian")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.User == null)
-            {
-                return Problem("Entity set 'RAD_bibliotekaContext.User'  is null.");
-            }
-            var user = await _context.User.FindAsync(id);
-            if (user != null)
-            {
-                _context.User.Remove(user);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    if (_context.User == null)
+        //    {
+        //        return Problem("Entity set 'RAD_bibliotekaContext.User'  is null.");
+        //    }
+        //    var user = await _context.User.FindAsync(id);
+        //    if (user != null)
+        //    {
+        //        _context.User.Remove(user);
+        //    }
 
-        private bool UserExists(int id)
-        {
-          return (_context.User?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        //private bool UserExists(int id)
+        //{
+        //  return (_context.User?.Any(e => e.Id == id)).GetValueOrDefault();
+        //}
     }
 }
