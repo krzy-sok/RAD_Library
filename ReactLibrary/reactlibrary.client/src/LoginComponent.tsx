7c9c51b277be76ba13fbed2d3c471535c2b465a5
ksokol <01178595@pw.edu.pl>
2025-01-06 21:45:42 +0100

    login wrapper function, preparations for useContext

--- ReactLibrary/reactlibrary.client/src/LoginComponent.tsx
+++ ReactLibrary/reactlibrary.client/src/LoginComponent.tsx
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import { Header, Footer } from './shared/_Layout';
 import { useForm, FormProvider, useFormContext } from "react-hook-form";
 import { InputField } from "./shared/InputField"
+
+//type AuthContext = {
+//    role?: string | null
+//    username?: string | null
+//};
+
+//const authContext = createContext<AuthContext | undefined>(undefined)
+
+//type AuthProviderProps = PropsWithChildren
+
 
 export const LoginForm = () => {
     const methods = useForm();
@@ -11,7 +21,7 @@ export const LoginForm = () => {
         console.log(data)
         //make put request to /user
 
-        SendLoginRequest(data);
+       LoginWrapper(data);
     })
     return (
         <div>
@@ -38,24 +48,54 @@ export const LoginForm = () => {
             <Footer />
         </div>
     );
-
-    async function SendLoginRequest(data) {
-        console.log("\n********\n\n in send request \n\n ******\n");
-        console.log(data)
-        const requestOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        };
-        const response = await fetch('/user/login', requestOptions);
-        if (response.ok) {
+    async function LoginWrapper(data) {
+        const result = await SendLoginRequest(data)
+        if (result == 200) {
             setFeedback(<div style={{ color: "green" }} > Login succesful login</div>);
         }
-        else if (response.status == 406) {
+        else if (result == 406) {
             setFeedback(<div style={{ color: "red" }} >Incorrect login data</div>)
         }
         else {
             setFeedback(<div style={{ color: "red" }} >Bad request</div>)
         }
     }
+
 }
+
+async function SendLoginRequest(data) {
+    console.log("\n********\n\n in send request \n\n ******\n");
+    console.log(data)
+    const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    };
+    const response = await fetch('/user/login', requestOptions);
+    if (response.ok) {
+        return 200;
+    }
+    else if (response.status == 406) {
+        return 406;
+    }
+    else {
+        return 400;
+    }
+}
+
+//export default function AuthProvider({ children }: AuthProviderProps) {
+//    const [role, setRole] = useState<string | undefined>();
+//    const [username, setUsername] = useState<string | undefined>();
+
+//    return <authContext.Provider value={ }>{children}</authContext.Provider>
+//}
+
+//export function useAuth() {
+//    const context = useContext(authContext);
+
+//    if (context === undefined) {
+//        throw new Error("Use auth must be used inside of auth provider");
+//    }
+
+//    return context;
+//}
--- ReactLibrary/reactlibrary.client/src/main.tsx
+++ ReactLibrary/reactlibrary.client/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, createContext } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
 import "./css/bootstrap.min.css"
@@ -8,8 +8,8 @@ import { Catalogue } from "./Catalogue.tsx"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { BookDetails, BookDelete } from "./BookCrud.tsx"
 import { BookEdit, BookCreate } from "./BookEdit.tsx"
-import { LoginForm } from "./LoginForm.tsx"
-import { RegisterForm } from "./RegisterForm.tsx"
+import { LoginForm } from "./LoginComponent.tsx"
+import { RegisterForm } from "./RegisterComponent.tsx"
 
 
 createRoot(document.getElementById('root')!).render(
